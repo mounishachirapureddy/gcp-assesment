fd258df16971acd7775afb72a774396c3b6a3f8b
mounishachirapureddy <137499284+mounishachirapureddy@users.noreply.github.com>
2024-12-21 21:51:53 +0530

    Create main.tf

---
+++ module/vpc/main.tf
@@ -0,0 +1,19 @@
+resource "google_project_service" "compute" {
+  service = "compute.googleapis.com"
+}
+
+resource "google_project_service" "container" {
+  service = "container.googleapis.com"
+}
+
+resource "google_compute_network" "main" {
+  name                            = var.network_name
+  routing_mode                    = "REGIONAL"
+  auto_create_subnetworks         = false
+  delete_default_routes_on_create = false
+
+  depends_on = [
+    google_project_service.compute,
+    google_project_service.container
+  ]
+}
