86061cf594579cc26bec733dabfefde2c3244083
mounishachirapureddy <137499284+mounishachirapureddy@users.noreply.github.com>
2024-12-21 21:56:22 +0530

    Update variables.tf

--- module/vpc/variables.tf
+++ module/vpc/variables.tf
@@ -1,8 +1,3 @@
-variable "region" {
-  description = "The region where resources will be created."
-  type        = string
-}
-
 variable "network_name" {
   description = "The name of the VPC network."
   type        = string
